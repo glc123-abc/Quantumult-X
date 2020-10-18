55d77aba945b796aafe8b7e8a994509088a603fe
glc123-abc <69333963+glc123-abc@users.noreply.github.com>
2020-10-18 23:34:56 +0800

    Update henpi.js

--- NobyDa/Surge/JS/henpi.js
+++ NobyDa/Surge/JS/henpi.js
@@ -1,9 +1,5 @@
 let obj = JSON.parse($response.body);
-
-const vip = '/app/subscribe/ranking'; 
-if (url.indexOf(vip) != -1) { 
-obj.["is_need_pay"] = 0; 
-obj.["isNeedAdvUnlock"] = "no"; 
-body = JSON.stringify(obj); 
+obj{"is_need_pay" = 0; 
+"isNeedAdvUnlock" = "no"; 
 }
-$done({body});
+$done({body: JSON.stringify(obj)});
