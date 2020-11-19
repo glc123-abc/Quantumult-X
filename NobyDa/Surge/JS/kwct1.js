64f9fda21d9059b04983abb2cf6a8bc8c4981638
glc123-abc <69333963+glc123-abc@users.noreply.github.com>
2020-11-20 02:27:55 +0800

    Update kwct1.js

--- NobyDa/Surge/JS/kwct1.js
+++ NobyDa/Surge/JS/kwct1.js
@@ -2,14 +2,10 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/a.p';
+const vip = '/a.p?';
 
 if (url.indexOf(vip) != -1) {
 	obj.data["packs"] = {"type":2,"end":1800000000,"period":111,"bought_vip":1,"bought_vip_end":1800000000};
-	obj.data["limitfree"] = 1;
-	obj.data["playright"] = 1;
-  obj.data["downright"] = 1;
-  obj.data["policytype"] = 1;
 	body = JSON.stringify(obj);
 }
 
